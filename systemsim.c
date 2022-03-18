e23278c788495af7555fd46dc995133f9842b881
Kutay Demiray <kutaydemiray921@gmail.com>
2022-03-18 16:52:44 +0300

    Created systemsim.c skeleton
    Defined control threads

---
+++ systemsim.c
@@ -0,0 +1,22 @@
+#include "cl_args.h"
+#include "process.h"
+
+// process generator thread
+void *process_generator(void *args) {
+
+}
+
+// cpu scheduler thread
+void *cpu_scheduler(void *args) {
+
+}
+
+int main(int argc, char **argv) {
+	// process args
+	
+	// simulate stuff
+	
+	return 0;
+}
+
+
