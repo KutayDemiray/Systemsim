1599a91dc1fec0e88baaf240e7f815413d2288a7
Kutay Demiray <kutaydemiray921@gmail.com>
2022-03-29 20:58:26 +0300

    Update process.h

--- Systemsim/process.h
+++ Systemsim/process.h
@@ -9,11 +9,15 @@
 #include "pcb_queue.h"
 
 typedef struct {
-
+	// TODO possibly a pointer to the current process in the cpu
 } process_arg;
 
 // simulated process as a thread
 static void *process_th(void *args) {
 	// TODO
+	
+	// TODO cpu scheduler wakes up all processes when it picks one to run
+	// so all processes should check whether they're the ready process or not when they run
+	
 }
 
